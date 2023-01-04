d0a098a441997c1fcc670f596cf84a8d30cf397e
Henry Okeke <iamhenryokeke@gmail.com>
2023-01-04 13:40:38 +0100

    added logic for temperature conversion and displaying weather cloud and cloud icon

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,45 @@
-const btn = document.querySelector("button");
+const searchBtn = document.querySelector("button");
+const inputForm = document.querySelector("form");
 
 async function getWeatherData() {
     let cityName = document.querySelector("input").value;
     cityName === ""
         ? (cityName = "Lagos")
         : (cityName = document.querySelector("input").value);
-    console.log(cityName);
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=74e5cd0c54200e60787922b9af13646d`
     const errorMessage = document.querySelector("span.error");
     try {
         const response = await fetch(apiURL, { mode: "cors" });
         const weatherData = await response.json();
         errorMessage.textContent = "";
-        console.log(weatherData);
         console.log(getNeededData(weatherData));
         populate(getNeededData(weatherData));
 
     } catch (err) {
         const response = await fetch(apiURL, { mode: "cors" });
         const weatherData = await response.json();
-        errorMessage.textContent = weatherData.message;
+        errorMessage.textContent = weatherData.message + '. Search must be in the form of "City", "City, State" or "City, Country".';
         console.log("Error!!!!!!!!!!!!!!");
     }
 }
 
-function getNeededData(data) {
+const getNeededData = (data) => {
     let tempInCelsius = (data.main.temp - 273).toFixed(2)
     let feelsLikeInCelsius = (data.main.feels_like - 273).toFixed(2)
     return {
         location: data.name,
         temp: tempInCelsius,
+        cloud: data.weather[0].description,
+        cloudImg: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
         feelsLike: feelsLikeInCelsius,
         humidity: data.main.humidity,
         windSpeed: data.wind.speed
     };
+}
+
+const toggleTempUnit = () => {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("tempToggle");
 }
 
 const populate = (obj) => {
@@ -42,14 +48,30 @@ const populate = (obj) => {
     let feelsLike = document.getElementById("feels");
     let humidity = document.getElementById("humidity");
     let windSpeed = document.getElementById("wind-speed")
+    let cloud = document.getElementById("cloud");
+    let cloudIcon = document.getElementById("cloud-icon")
 
-    location.textContent = `${obj.location}`;
-    temperature.textContent = `${obj.temp}`;
-    feelsLike.textContent = `${obj.feelsLike}`;
-    humidity.textContent = `${obj.humidity} %`;
-    windSpeed.textContent = `${obj.windSpeed}`;
+    location.innerHTML = `${obj.location}`;
+    cloud.innerHTML = `Cloud: ${obj.cloud}`;
+    cloudIcon.src = obj.cloudImg;
+    temperature.innerHTML = `${obj.temp}&#176;C`;
+    feelsLike.innerHTML = `It feels likes ${obj.feelsLike}&#176;C`;
+    humidity.innerHTML = `Humidity: ${obj.humidity}%`;
+    windSpeed.innerHTML = `Wind Speed: ${obj.windSpeed} m/s`;
 }
+
+const toCelsius = (fahrenheit) => {
+    return (5 / 9) * (fahrenheit - 32);
+}
+
+const toFahrenheit = (celsius) => {
+    return ((9 / 5) * celsius) + 32;
+}
+
+inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
 
 getWeatherData()
 
-btn.addEventListener("click", getWeatherData);
+searchBtn.addEventListener("click", getWeatherData);
